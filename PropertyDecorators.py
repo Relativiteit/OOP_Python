7870ad282838c0d058c242b648d177f847fbda4d
Relativiteit <alejocain@gmail.com>
2021-10-23 03:20:02 +0200

    ClassMethods, Decorators

--- PropertyDecorators.py
+++ PropertyDecorators.py
@@ -8,20 +8,20 @@ class Employee:
 
     @property
     def email(self):  # self is a instance argument to create the object.
-        return '{} {}@email.com'.format(self.first, self.last)
+        return '{}.{}@email.com'.format(self.first, self.last)
 
     @property
     def fullname(self):  # self is a instance argument to create the object.
         return '{} {}'.format(self.first, self.last)
-# setter
 
+# setter
     @fullname.setter
     def fullname(self, name):
         first, last = name.split(' ')
         self.first = first
         self.last = last
-# deleter
 
+# deleter
     @fullname.deleter
     def fullname(self):
         print('Delete Name!')
@@ -39,6 +39,4 @@ emp_1.fullname = 'Corey Schafer'
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname)
-print(help(Employee))
-print(dev_1.email)
-print(dev_2.email)
+# print(help(Employee))
