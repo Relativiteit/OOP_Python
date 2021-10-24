a383481f38a2ce7114dabf95004bb03f3d991fa2
Relativiteit <alejocain@gmail.com>
2021-10-24 06:55:05 +0200

    functions as decorators

--- Decorators.py
+++ Decorators.py
@@ -1,16 +1,22 @@
 # Decorators
 
 def decorator_function(orginal_function):
-    def wrapper_function():
+    def wrapper_function(*args, **kwargs):  # any arguments, any keywords or functions
         print('wrapper executed this before {}'.format(orginal_function.__name__))
-        return orginal_function()
+        return orginal_function(*args, **kwargs)
     return wrapper_function
 
 
+@decorator_function
 def display():
     print('disp[lay function ran')
 
 
-decorated_display = decorator_function(display)
+@decorator_function
+def display_info(name, age):
+    print('display_info ran with arguments ({}, {})'.format(name, age))
 
-decorated_display()
+
+display_info('John', 25)
+
+display()
