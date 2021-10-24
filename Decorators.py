fa381bc48cb5be5b11dc579e246ad4a085f88e7c
Relativiteit <alejocain@gmail.com>
2021-10-24 08:04:37 +0200

    decoratos and log files

--- Decorators.py
+++ Decorators.py
@@ -1,30 +1,31 @@
 # Decorators
 
 def decorator_function(original_function):
-    def wrapper_function(*args, **kwargs):  # any arguments, any keywords or functions
+    # any arguments, any keywords arguments or functions
+    def wrapper_function(*args, **kwargs):
         print('wrapper executed this before {}'.format(
             original_function.__name__))
         return original_function(*args, **kwargs)
     return wrapper_function
 
 
-class decorator_class(object):
+# class decorator_class(object):
 
-    def __init__(self, original_function):
-        self.original_function = original_function
+#     def __init__(self, original_function):
+#         self.original_function = original_function
 
-    def __call__(self, *args, **kwargs):
-        print('call method executed this before {}'.format(
-            self.original_function.__name__))
-        return self.original_function(*args, **kwargs)
+#     def __call__(self, *args, **kwargs):
+#         print('call method executed this before {}'.format(
+#             self.original_function.__name__))
+#         return self.original_function(*args, **kwargs)
 
 
-@decorator_class
+@decorator_function
 def display():
     print('disp[lay function ran')
 
 
-@decorator_class
+@decorator_function
 def display_info(name, age):
     print('display_info ran with arguments ({}, {})'.format(name, age))
 
